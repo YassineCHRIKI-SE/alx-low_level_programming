b6de6f043155d779d25492d0e5a8446e405857d5
YassineCHRIKI-SE <yassin.chriki@gmail.com>
2024-01-24 06:59:50 -0800

    a

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- *  * main - get_op_func has operators correlated with
- *   * func signs and funcs from op_func
- *    * if not 4 arguments, return Error & exit 98
- *     * if op is null, return Error & exit 99
- *      * if div or mod 0, return Error & exit 100
- *       * run calc, input one, operator, input two = pointer res to get_op
- *        * @argc: arguments
- *         * @argv: double pointer to arguments
- *          * Return: 0
+ ** main - get_op_func has operators correlated with
+ ** func signs and funcs from op_func
+ ** if not 4 arguments, return Error & exit 98
+ ** if op is null, return Error & exit 99
+ ** if div or mod 0, return Error & exit 100
+ ** run calc, input one, operator, input two = pointer res to get_op
+ ** @argc: arguments
+ ** @argv: double pointer to arguments
+ ** Return: 0
 **/
 int main(int argc, char *argv[])
 {
-	int on;
-	int two; 
-	int ans;
-	
+	int on, two, ans;
 	int (*res)(int, int);
 	char *get_op;
 
@@ -26,11 +23,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	one = atoi(argv[1]);
 	two = atoi(argv[3]);
 	get_op = argv[2];
-
 	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 	{
 		printf("Error\n");													exit(99);
@@ -40,7 +35,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(100);
 	}
-
 	res = get_op_func(get_op);
 	ans = res(one, two);
 	printf("%d\n", ans);
